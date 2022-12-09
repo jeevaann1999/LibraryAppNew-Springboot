04bdf5cf2a58a441680f8f896042d18a2889c672
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-09 05:45:07 +0530

    first commit

---
+++ src/main/java/com/example/libraryappnew_backend/LibraryappnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryappnew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryappnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryappnewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libraryappnew_backend/LibraryappnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryappnew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
